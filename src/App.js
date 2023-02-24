b7e7b96f2ce6761286848f8ea2c39de4f254d3f2
Kazenzi <jacintakazenzi99@gmail.com>
2023-02-24 16:06:11 +0300

    day two

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
+import Welcome from './components/Welcome';
+import Hello from './components/Hello';
+import Message from './components/Message';
+
 
 function App() {
   return (
     <div className="App">
+     
       <Greet/>
+     <Welcome/>
+     <Hello/>
+     <Message/>
       
     </div>
   );
